6c4c0405a0c61331f9b7bacd97415de1a81620d4
code House <mianalikhalid2000@gmail.com>
2024-06-28 09:06:22 +0500

    Add contact us

---
+++ src/Assests/Components/Contactus.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../Css/Contactus.css'   
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+
+const Contactus = () => {
+  return (
+    <div>
+        
+      
+    </div>
+  )
+}
+export default Contactus
